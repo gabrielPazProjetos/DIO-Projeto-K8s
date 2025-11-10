91019cea04e851f56daf24fb56484197416116de
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-10 15:03:51 -0300

    Create conexao.php

---
+++ app/conexao.php
@@ -0,0 +1,12 @@
+<?php
+$host = getenv('DB_HOST') ?: 'localhost';
+$user = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: '';
+$database = 'comentarios';
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
+}
+?>
